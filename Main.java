7b9b734a15df44bcef988286927bcd1e65f46b22
Orca <stephanie.hu2@student.tdsb.on.ca>
2025-08-10 17:34:03 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String input;
+        
+        System.out.println("Hello! I'm QAIyum, a robot version of Mr. Qayum. I amm realy goood aat speling.");
+        
+        while (true) {
+        input = keyboard.nextLine().toLowerCase().trim();
+        
+            if (input.equals("one banana")) {
+                twoBanana();
+            }
+            
+            if (Math.random() <= 0.49) { // random from .0 to .99
+                oneBanana(); // 0.49 is 50% chance
+            }
+        }
+    }
+    
+    public static void twoBanana() {
+        System.out.println("Two banana.");
+    }
+    
+    public static void oneBanana() {
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("One banana.");
+        
+        while (true) {
+            String input = keyboard.nextLine().toLowerCase().trim();
+            if (input.equals("two banana")) {
+                System.out.println("One pound! \n");
+                break;
+            }
+        }
+    }
+}

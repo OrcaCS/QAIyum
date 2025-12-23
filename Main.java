ebd3da108bac2df881b1d6405733d3559e95d36c
Orca <stephanie.hu2@student.tdsb.on.ca>
2025-12-22 22:43:05 -0500

    Enhance input handling and random responses

--- Main.java
+++ Main.java
@@ -12,10 +12,34 @@ class Main {
         
             if (input.equals("one banana")) {
                 twoBanana();
-            }
-            
-            if (Math.random() <= 0.49) { // random from .0 to .99
+            } else if (input.contains("selfie")) {
+                System.out.println("Sending selfie...");
+            } else {
+                if (Math.random() <= 0.19) { // random from .0 to .99
                 oneBanana(); // 0.49 is 50% chance
+                } else {
+                    if (Math.random() <= 0.19) {
+                    System.out.println("Chooose between pokemon andd transformers.");
+                        input = keyboard.nextLine().toLowerCase().trim();
+                        if (input.contains("pokemon") & !input.contains("transformers")) {
+                            System.out.println("This is what's wrong with society.");
+                        } else {
+                            System.out.println("I hate pokemon.");
+                        }
+                    } else if ((Math.random() > 0.19 ) & (Math.random() <= 0.39 )) {
+                        System.out.println("Should Canada be the 51st state?");
+                    } else if ((Math.random() > 0.39 ) & (Math.random() <= 0.54 )) {
+                        System.out.println("What do you think about the current state of inflation due to excessive money printing?");
+                    } else if ((Math.random() > 0.54 ) & (Math.random() <= 0.70 )) {
+                        System.out.println("Have you seen Canada's population triangle? Aren't you concerned about the ratio of working class people to non-working class people?");
+                    } else if ((Math.random() > 0.70 ) & (Math.random() <= 0.76 )) {
+                        System.out.println("I sleepp a heallthy amonut!! I probably get mor sleep then u!!!");
+                    } else if ((Math.random() > 0.76 ) & (Math.random() <= 0.80 )) {
+                        System.out.println("The government is brainwashing us regarding COVID-19. Be skeptical. Don't trust anything they say!!1!");
+                    } else {
+                        System.out.println("Live on aa farrm and make ur kidz workk!!!1!!");
+                    }
+                }
             }
         }
     }
@@ -31,9 +55,11 @@ class Main {
         
         while (true) {
             String input = keyboard.nextLine().toLowerCase().trim();
-            if (input.equals("two banana")) {
+            if (input.equals("two banana") || input.equals("two banana.") || input.equals("two banana!")) {
                 System.out.println("One pound!!1! \n");
                 break;
+            } else {
+                System.out.println("One banana.");
             }
         }
     }

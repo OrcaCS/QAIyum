73bc810963b06c48e48912258bfcfdae0f5b7572
Orca <stephanie.hu2@student.tdsb.on.ca>
2025-12-23 16:26:46 -0500

    Implement random image selection and update error message
    Randomly select an image path based on a generated random number and update error message.

--- SendSelfie.java
+++ SendSelfie.java
@@ -7,13 +7,25 @@ import java.io.IOException;
 
 public class SendSelfie {
     public static void main(String[] args) {
-        String imagePath = "qaiyumselfie.jpg";
+        String imagePath
+        double random = Math.random();
+        
+        if (random <=  0.24) {
+            imagePath = "qaiyumselfie1.jpg";
+        } else if ((random >  0.24) && (random <=  0.49)) {
+            imagePath = "qaiyumselfie2.jpg";
+        } else if ((random >  0.49) && (random <=  0.74)) {
+            imagePath = "qaiyumselfie3.jpg";
+        } else {
+            imagePath = "qaiyumselfie4.jpg";
+        }
+        
         BufferedImage img = null;
         
         try {
             img = ImageIO.read(new File(imagePath));
         } catch (IOException e) {
-            System.err.println("Sorry, I can't do that. I'm busy right now!");
+            System.err.println("Sorry, your crappy device can't receive images. And I'm also busy right now.");
             return;
         }
 

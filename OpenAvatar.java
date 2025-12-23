4e6179fac0fe0ce652678aa2f4c08ef170f58f16
Orca <stephanie.hu2@student.tdsb.on.ca>
2025-12-23 16:20:33 -0500

    Refactor OpenAvatar to use JFrame for image display

--- OpenAvatar.java
+++ OpenAvatar.java
@@ -1,17 +1,30 @@
-import java.awt.Desktop;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 
-public class OpenAvatar {
+public class ImageViewer {
     public static void main(String[] args) {
+        String imagePath = "qaiyumavatar.jpg";
+        BufferedImage img = null;
+        
         try {
-            File file = new File("qaiyumavatar.jpg");
-
-            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.OPEN)) {
-                Desktop.getDesktop().open(file);
-            }
+            img = ImageIO.read(new File(imagePath));
         } catch (IOException e) {
-            System.err.println("Couldn't open avatar.");
+            System.err.println("Could not open avatar.");
+            return;
         }
+
+        JFrame frame = new JFrame("Image Viewer");
+        ImageIcon icon = new ImageIcon(img);
+        JLabel label = new JLabel(icon);
+        
+        frame.add(label);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }

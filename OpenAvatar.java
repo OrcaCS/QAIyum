b49e135a99e40787b58482fab5a11bec8c359a3f
Orca <stephanie.hu2@student.tdsb.on.ca>
2025-12-23 22:53:29 +0000

    git commit

--- OpenAvatar.java
+++ OpenAvatar.java
@@ -5,21 +5,22 @@ import java.io.File;
 import java.io.IOException;
 
 public class OpenAvatar {
-    public static void main() {
-        String imagePath = "qaiyumavatar.jpg";
+    public static void main(String[] args) {
+        String imagePath = "src/qaiyumavatar.jpg";
         BufferedImage img = null;
-        
+
         try {
             img = ImageIO.read(new File(imagePath));
         } catch (IOException e) {
-            System.err.println("\nCould not open avatar. Wow! Looks like your device is so crappy that you can't even see me. What are you chatting with me on, a potato??");
+            System.err.println(
+                    "\nCould not open avatar. Wow! Looks like your device is so crappy that you can't even see me. What are you chatting with me on, a potato??");
             return;
         }
 
         JFrame frame = new JFrame("Image Viewer");
         ImageIcon icon = new ImageIcon(img);
         JLabel label = new JLabel(icon);
-        
+
         frame.add(label);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
--- SendSelfie.java
+++ SendSelfie.java
@@ -8,19 +8,19 @@ public class SendSelfie {
     public static void main(String[] args) {
         String imagePath;
         double random = Math.random();
-        
-        if (random <=  0.24) {
+
+        if (random <= 0.24) {
             imagePath = "qaiyumselfie1.jpg";
-        } else if ((random >  0.24) && (random <=  0.49)) {
+        } else if ((random > 0.24) && (random <= 0.49)) {
             imagePath = "qaiyumselfie2.jpg";
-        } else if ((random >  0.49) && (random <=  0.74)) {
+        } else if ((random > 0.49) && (random <= 0.74)) {
             imagePath = "qaiyumselfie3.jpg";
         } else {
             imagePath = "qaiyumselfie4.jpg";
         }
-        
+
         BufferedImage img = null;
-        
+
         try {
             img = ImageIO.read(new File(imagePath));
         } catch (IOException e) {
@@ -31,7 +31,7 @@ public class SendSelfie {
         JFrame frame = new JFrame("Image Viewer");
         ImageIcon icon = new ImageIcon(img);
         JLabel label = new JLabel(icon);
-        
+
         frame.add(label);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
